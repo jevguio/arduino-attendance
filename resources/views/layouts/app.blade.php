e278f2f8b976338ce9cb5381a9663f0772543eea
John Vincent S. Guioguio <jvguioguio@gmail.com>
2024-08-20 22:04:51 +0800

    first

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-<script src="{{ asset('build/assets/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
 
 </head>
 <body class="font-sans antialiased">
